d89278d09b91c94226bc3bad4b432a8c6cb45d5d
SolloVey <chelnokow@gmail.com>
2022-01-11 22:35:19 +0300

    Add style and development popup

--- script/app.js
+++ script/app.js
@@ -22,19 +22,69 @@ let formInputs = document.querySelectorAll('.js-input');
 let inputEmail = document.querySelector('.js-mail');
 let inputCheckbox = document.querySelector('.js-checkbox');
 
+function validateEmail (email) {
+   let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return re.test(String(email).toLowerCase());
+}
+
 form.onsubmit = function () {
    let emailVal = inputEmail.value;
+   let emptyInputs = Array.from(formInputs).filter(input => input.value === '');
 
    formInputs.forEach(function (input) {
         if (input.value === '') {
             input.classList.add('error');
-
+            console.log('inputs not filed');
         } else {
             input.classList.remove('error');
         }
     });
 
+   if (emptyInputs.length !== 0) {
+      console.log('inputs not filed');
+      return false;
+   }
 
+   if (!validateEmail(emailVal)) {
+      console.log('email not valid');
+      inputEmail.classList.add('error');
+      return false;
+   } else {
+      inputEmail.classList.remove('error');
+   }
+ 
+   if (!inputCheckbox.checked) {
+      console.log('checkbox not checked');
+      inputCheckbox.classList.add('error-checkbox');
+      return false;
+   } else {
+      inputCheckbox.classList.remove('error-checkbox');
+   }
+};
 
-   return false;
+// МОДАЛЬНОЕ ОКНО
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
 }
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
