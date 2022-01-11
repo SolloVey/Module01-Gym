527978b01774580a2abbfc1bcdc032752ccb1754
SolloVey <chelnokow@gmail.com>
2022-01-11 08:29:25 +0300

    Add style and start of validation development

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+// ПЛАВНЫЙ СКРОЛЛ ПО ССЫЛКЕ
+
 const links = document.querySelectorAll(".trans");
 
     for (const link of links) {
@@ -11,4 +13,28 @@ const href = this.getAttribute("href");
 document.querySelector(href).scrollIntoView({
    behavior: "smooth"
     });
+}
+
+// ВАЛИДАЦИЯ
+
+let form = document.querySelector('.js-form');
+let formInputs = document.querySelectorAll('.js-input');
+let inputEmail = document.querySelector('.js-mail');
+let inputCheckbox = document.querySelector('.js-checkbox');
+
+form.onsubmit = function () {
+   let emailVal = inputEmail.value;
+
+   formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error');
+
+        } else {
+            input.classList.remove('error');
+        }
+    });
+
+
+
+   return false;
 }
